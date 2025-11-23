28adb1ee58e3140c4ca04a666cd41c1c4a7f984d
adamgate <applegateadaml@gmail.com>
2025-11-22 20:47:32 -0700

    fix incorrect merge issue

--- MediaSorter/Constants/CommandLineConstants.cs
+++ MediaSorter/Constants/CommandLineConstants.cs
@@ -2,10 +2,8 @@
 {
     public static class CommandLineConstants
     {
-        public static readonly string[] TerminationCommands = ["Exit", "exit", "X", "x"];
-
-        public static readonly string[] ConfirmationCommands = ["Yes", "yes", "Y", "y"];
-
-        public static readonly string[] DeclineCommands = ["No", "no", "N", "n"];
+        public static readonly string[] ConfirmationCommands = ["Yes", "Y"];
+        public static readonly string[] DeclineCommands = ["No", "N"];
+        public static readonly string[] TerminationCommands = ["Exit", "X"];
     }
 }
